977e2ec2cc4dc0b3107d5a80a19e92969b357120
Simon Valentino <simontvalentino@gmail.com>
2023-10-17 20:40:07 -0400

    Added adding tasks and made it look better.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Todo(models.Model):
+    content = models.CharField(max_length=255, null=False)
+    created_on = models.DateTimeField(auto_now=True)
+    
+    def __repr__(self):
+        return f"Task: {self.content}"
--- core/views.py
+++ core/views.py
@@ -1,5 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Todo
 
 # Create your views here.
 def index(request):
-    return render(request, "core/index.html")
+    if request.method == "POST":
+        content = request.POST.get("content", "")
+
+        if content:
+            try:
+                task = Todo(content=content)
+                task.save()
+            except Exception as e:
+                return render(request, "core/error.html", {"error_message": str(e)})
+
+            return redirect("index")
+
+    tasks = Todo.objects.all().order_by("created_on")
+    return render(request, "core/index.html", {"tasks": tasks})
